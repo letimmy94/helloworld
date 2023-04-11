853a3d2cd201d398f67ab89b801b158fa2514d8a
Timmy Le <le.timmy94@gmail.com>
2023-04-11 07:51:07 -0400

    contact

---
+++ helloworld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  helloworld
+//
+//  Created by Timmy Le on 4/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
